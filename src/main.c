8f28b01cb62e0236275e8bd97642869acaad2206
svangc <christi448@gmail.com>
2025-10-20 23:35:37 -0700

    Initial commit

--- src/main.c
+++ src/main.c
@@ -1,1 +1,17 @@
+#include <userint.h>
+#include "synchro.h"
 
+int main(void){
+	int panel; 
+	
+	//load user interface file
+	if ((panel = LoadPanel(0, "synchro.uir", PANEL)) < 0)
+		return -1; 
+	
+	DisplayPanel(panel);
+	RunUserInterface();
+	DiscardPanel(panel);
+	
+	return 0;
+	
+}
